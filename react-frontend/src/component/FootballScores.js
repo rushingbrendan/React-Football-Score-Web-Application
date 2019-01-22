829582ee6e4c8f02f18b2ea8d8c9233800fe9c58
brushing <brushing@outlook.com>
2019-01-22 17:33:09 -0500

    added example table for scores

--- react-frontend/src/component/FootballScores.js
+++ react-frontend/src/component/FootballScores.js
@@ -95,6 +95,50 @@ return (
         </div>
     )}
 
+    
+    <div className="footballScoreContainer">
+    <table border="0px" className="footballScoreTableTop">
+        <tr className="footballScoreSpaceAboveQuarters"></tr>
+        <tr>            
+            <td><div className="footballScoreHeaderSpace"></div></td>
+            <td><div className="footballQuarterScoreFont">1</div></td>
+            <td><div className="footballQuarterScoreFont">2</div></td>
+            <td><div className="footballQuarterScoreFont">3</div></td>
+            <td><div className="footballQuarterScoreFont">4</div></td>
+            <td><div className="footballQuarterScoreFont">OT</div></td>    
+            <td><div className="footballScoreHeaderTotalSpace"></div></td>        
+        </tr>
+    </table>
+    <div className="spaceUnderQuarterTitles"></div>
+    <hr className="footballScoreLineBreak"></hr>
+    <table border="0px">
+        <tr>            
+            <td><div className="footballTeamNameFont">New England</div></td>
+            <td><div className="footballQuarterScoreFont">7</div></td>
+            <td><div className="footballQuarterScoreFont">7</div></td>
+            <td><div className="footballQuarterScoreFont">3</div></td>
+            <td><div className="footballQuarterScoreFont">14</div></td>
+            <td><div className="footballQuarterScoreFont">6</div></td>            
+            <td><div className="footballTotalScoreFont">37</div></td>    
+        </tr>
+        <tr>            
+            <td><div className="footballTeamNameFont">Kansas City</div></td>
+            <td><div className="footballQuarterScoreFont">0</div></td>
+            <td><div className="footballQuarterScoreFont">0</div></td>
+            <td><div className="footballQuarterScoreFont">7</div></td>
+            <td><div className="footballQuarterScoreFont">24</div></td>
+            <td><div className="footballQuarterScoreFont">0</div></td>   
+            <td><div className="footballTotalScoreFont">31</div></td>           
+        </tr>
+        
+        
+    </table>
+    
+
+
+    </div>
+
+
 
 <div className="spaceUnderTeamData"></div>
     <h1 className="sportsScoreTitle">Teams</h1>
